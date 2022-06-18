8bb39a0d8f24f6306d876675e1b7d7d1bd16f542
milena M <mihajloska00@gmail.com>
2022-06-18 22:25:47 +0200

    Initial commit

---
+++ src/Milena/HeyYou.java
@@ -0,0 +1,14 @@
+package Milena;
+
+import java.lang.annotation.Documented;
+
+public class HeyYou {
+    public static void main(String... args) {
+        String x = "Hey, You";
+        System.out.println();
+    public HeyYou() ;
+
+        }
+        }
+    }
+
